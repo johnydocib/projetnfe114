b7e27e24d40c055dd67b122566f0c5631b714b2e
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 14:11:16 +0100

    gg

--- liste.php
+++ liste.php
@@ -13,6 +13,8 @@ $base->exec("SET CHARACTER SET utf8");
 //2° - Préparation de requette et execution
 $retour = $base->query('SELECT * FROM movies;');
 
+
+
 //3° - Lecture du resultat de la requette
 while ($data = $retour->fetch()){
 echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
