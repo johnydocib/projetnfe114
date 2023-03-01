a5b21bf377a2ea450d2bfa647ae49e493fe4b946
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 15:32:31 +0100

    assq

--- recherche.php
+++ recherche.php
@@ -8,6 +8,7 @@ $base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400
 $base->exec("SET CHARACTER SET utf8");
 
 //2° - Préparation de requette et execution
+$retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
 $retour = $base->query('SELECT DISTINCT annee FROM movies;');
 
 //3° - Lecture du resultat de la requette
