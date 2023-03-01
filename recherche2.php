c6c083fa1d1546142be16a2ea0eea21641c30cfa
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 15:29:05 +0100

    qsq

--- recherche2.php
+++ recherche2.php
@@ -8,8 +8,8 @@ $ANNEE = $_GET['annee'];
     $base->exec("SET CHARACTER SET utf8");
     
     //2° - Préparation de requette et execution
-    // $retour = $base->query('SELECT DISTINCT annee FROM movies;');
-    $retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
+     $retour = $base->query('SELECT DISTINCT annee FROM movies;');
+    //$retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
     
     //3° - Lecture du resultat de la requette
     while ($data = $retour->fetch()){
