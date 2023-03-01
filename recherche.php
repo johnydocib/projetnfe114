d4c210dd17029414f2b0e2054e384f1848f68c28
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 12:46:13 +0100

    ffd

---
+++ recherche.php
@@ -0,0 +1,19 @@
+<p>ANNEE ? <SELECT name="annee">
+
+    <?php
+    
+    //1° - Connexion à la BDD
+    $base = new PDO('mysql:host=localhost; dbname=id20206067_movies', 'id20206067_docib', 'Carnet741852@');
+    $base->exec("SET CHARACTER SET utf8");
+    
+    //2° - Préparation de requette et execution
+    $retour = $base->query('SELECT DISTINCT annee FROM movies;');
+    
+    //3° - Lecture du resultat de la requette
+    while ($data = $retour->fetch()){
+    echo "<option>".$data['annee']."</option>";
+    }
+    
+    ?>
+    </SELECT>
+    </p>
