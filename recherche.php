fe4011f546e2c7721ba559088dc50e7805332568
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 15:22:31 +0100

    asqq

--- recherche.php
+++ recherche.php
@@ -11,6 +11,26 @@
 
 
     <SELECT name="annee"><br> 
+    <?php
 
+  
+$ANNEE = $_GET['annee'];
+
+    //1° - Connexion à la BDD
+    $base = new PDO('mysql:host=localhost; dbname=id20206067_movies', 'id20206067_docib', 'Carnet741852@');
+    $base->exec("SET CHARACTER SET utf8");
+    
+    //2° - Préparation de requette et execution
+    // $retour = $base->query('SELECT DISTINCT annee FROM movies;');
+    $retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
+    
+    //3° - Lecture du resultat de la requette
+    while ($data = $retour->fetch()){
+    echo "<option>".$data['annee']."</option>";
+   
+    
+    }
+    
+    ?>
     </SELECT>
     </p>
