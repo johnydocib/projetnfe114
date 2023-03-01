cb35becb113afb23a99295ed5bdb543f71163a05
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 15:08:43 +0100

    npol

--- recherche.php
+++ recherche.php
@@ -1,6 +1,6 @@
 <p>ANNEE ? 
    <SELECT name="annee"><br> 
-   <h1>Recherche de l'année</h1>
+   <h1>Recherche de l'anné</h1>
     <form action="recherche2.php">
         <p><input type="submit"/></p>
     </form>
