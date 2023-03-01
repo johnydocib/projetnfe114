b873bc64599d8a33d2c909c6a59070d981da5761
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 15:30:31 +0100

    assq

--- recherche.php
+++ recherche.php
@@ -1,17 +1,19 @@
-<html>
-    <body>
-        
-    
+<p>ANNEE ? <SELECT name="annee">
 
-   <h1>Recherche de l'année</h1>
-    <form action="recherche2.php">
-        <p><input type="submit"/></p>
-    </form>
-   
-    
-    
+<?php
 
-    </body>
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400_boris', '=nq^^=vKz?FRiz7-');
+$base->exec("SET CHARACTER SET utf8");
 
-   
- </html>
+//2° - Préparation de requette et execution
+$retour = $base->query('SELECT DISTINCT annee FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo "<option>".$data['annee']."</option>";
+}
+
+?>
+</SELECT>
+</p>
