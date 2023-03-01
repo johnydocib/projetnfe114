f88e9a346bf4ccfb0ec31833b1ff6556c40b8925
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 14:59:33 +0100

    azss

--- recherche2.php
+++ recherche2.php
@@ -7,7 +7,8 @@
     $base->exec("SET CHARACTER SET utf8");
     
     //2° - Préparation de requette et execution
-    $retour = $base->query('SELECT DISTINCT annee FROM movies;');
+    // $retour = $base->query('SELECT DISTINCT annee FROM movies;');
+    $retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
     
     //3° - Lecture du resultat de la requette
     while ($data = $retour->fetch()){
