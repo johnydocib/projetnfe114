af43496260bd6318ab334d5709e091983f0cf018
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 14:47:36 +0100

    ssssszs

--- recherche.php
+++ recherche.php
@@ -10,10 +10,12 @@
 
     <?php
 
+    $TITRE= $_GET['titre'];
+    $GENRE = $_GET['genre'];
     $ANNEE = $_GET['annee'];
 
     echo $TITRE." ".$GENRE." ".$ANNEE; 
-    
+
     //1° - Connexion à la BDD
     $base = new PDO('mysql:host=localhost; dbname=id20206067_movies', 'id20206067_docib', 'Carnet741852@');
     $base->exec("SET CHARACTER SET utf8");
