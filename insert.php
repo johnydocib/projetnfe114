189adb54ee793e7ece30967f31447f252c2bb227
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-01 12:55:16 +0100

    gg

--- insert.php
+++ insert.php
@@ -10,7 +10,7 @@ $ANNEE = $_GET['annee'];
 echo $TITRE." ".$GENRE." ".$ANNEE; 
 
 //1° - Connexion à la BDD
-$base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400_boris', '=nq^^=vKz?FRiz7-');
+$base = new PDO('mysql:host=localhost; dbname=id20206067_movies', 'id20206067_docib', 'Carnet741852@');
 $base->exec("SET CHARACTER SET utf8");
 $sql = 'INSERT INTO movies VALUES(NULL,"'.$TITRE.'","'.$GENRE.'",'.$ANNEE.')';
 //2° - Préparation de requette et execution
