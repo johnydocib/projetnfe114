160781681d3933e841b5d2dceae8cbfe4c14a11a
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-02 17:15:22 +0100

    yete

--- carte.php
+++ carte.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <link rel="stylesheet" href="/path/to/leaflet.css" />
+<script src="/path/to/leaflet.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
     <title>Afficher une carte</title>
@@ -10,6 +14,8 @@
         width: 100%;
       }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" />
   </head>
   <body>
     <form>
@@ -24,59 +30,15 @@
       function afficherCarte() {
         var lat = document.getElementById("latitude").value;
         var long = document.getElementById("longitude").value;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: parseFloat(lat), lng: parseFloat(long)},
-          zoom: 8
-        });
-        var marker = new google.maps.Marker({
-          position: {lat: parseFloat(lat), lng: parseFloat(long)},
-          map: map,
-          title: 'Ma position'
-        });
+        var map = L.map('map').setView([parseFloat(lat), parseFloat(long)], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+          maxZoom: 18,
+          tileSize: 512,
+          zoomOffset: -1
+        }).addTo(map);
+        var marker = L.marker([parseFloat(lat), parseFloat(long)]).addTo(map);
       }
     </script>
-    <script src="https://openlayers.org/api/OpenLayers.js"
-    async defer></script>
   </body>
-
-
-
-
-
-
-
-<script>
-function envoyer(){
-    //Récuppération des valeurs saisies pas l'utilisateur
-	var rue = document.getElementById('rue').value;
-    var ville = document.getElementById('code').value;
-    
-	//API issu de https://adresse.data.gouv.fr/api-doc/adresse
-	
-	//Création de l'object Ajax
-	var req= new XMLHttpRequest();
-    //Préparation de requette
-	req.open("GET","https://api-adresse.data.gouv.fr/search/?q="+rue+"&postcode="+ville,false); 
-    //Envois de l'information au serveur
-	req.send(null); 
-    //Reception et affichage du résultat
-	var geocodeObjet = JSON.parse(req.responseText);
-    
-    //Filtre pour récuppérer uniquement les valeurs GPS de l'adresse
-    var long = geocodeObjet.features[0].geometry.coordinates[0];
-    var lat = geocodeObjet.features[0].geometry.coordinates[1];
-
-    document.getElementById('result').innerHTML="Long "+long+"</br>Lat "+lat;
-    
-}    
-</script>
-
-   
-
-<p>Adresse <input id="rue"/></p>
-<p>Code postal <input id="code"/></p>
-<button onclick="envoyer()">Envoyer</button>
-<p id="result"></p>
-
-</body>
 </html>
