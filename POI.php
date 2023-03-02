476907dbf8a3a8aa7ff043bc3ee08697caeee240
johnydocib <102001647+johnydocib@users.noreply.github.com>
2023-03-02 12:53:52 +0100

    deez

--- POI.php
+++ POI.php
@@ -1,6 +1,6 @@
 <?php
 
-//C'est le POI de l'utilisateur
+//C'est le POI de l'utilisateu
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
 echo "43.296482\t5.36978\tMoi\tMa Position\tpngegg.png\t24,24\t0,-24\n";
 
